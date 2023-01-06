086ac3f3502bb5557a4d7eaef6db1015a35424e5
ademalabas@gmail.com <ademalabas@gmail.com>
2023-01-06 20:49:19 +0300

    06.01.23

--- src/test/java/clarusway/utilities/Driver.java
+++ src/test/java/clarusway/utilities/Driver.java
@@ -3,6 +3,8 @@ package clarusway.utilities;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import java.time.Duration;
 
@@ -17,9 +19,21 @@ private static WebDriver driver;
 
     public static WebDriver getDriver(){// private oldugu için getterini oluşturduk
 
+
+        String browser =ConfigReader.getProperty("browser");
+
         if(driver == null){
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
+            switch (browser) {
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+
+            }
             driver.manage().window().maximize();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         }
